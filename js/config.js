60ee27cf0691ed00eebbaee5de3832e957399f1f
2p94nzg5g9-pixel <2p94nzg5g9@privaterelay.appleid.com>
2026-08-30 14:04:34 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window.SITE_CONFIG = Object.freeze({
+  siteName: "TAKUMI apps",
+  developerName: "TAKUMI apps",
+  contactEmail: "madaoganbaru@gmail.com",
+  appStoreUrls: {
+    reshimamo: ""
+  }
+});
